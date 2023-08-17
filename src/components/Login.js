e62b628eece94e092b856c05bd72f7bbf3d80a13
harir8281 <harikrishnan.r@enfintechnologies.com>
2023-08-17 09:45:30 +0530

    intial commit

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+    const loginHandler = (event) => {
+        console.log(event.target.value)
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
